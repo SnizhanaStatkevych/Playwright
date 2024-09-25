6c7ce5157efd7db5cdb1070e0d933042b2a6d903
snizhanastatkevych <snizhana.statkevych1501@gmail.com>
2024-09-25 20:59:32 +0300

    Додано автотести до Playwright

---
+++ basicsJSforQA.js
@@ -0,0 +1,34 @@
+/* console.log("Hello World")
+
+//there are comment 
+let a = 4;
+console.log(typeof(a))
+
+
+let b = "Snizhana"
+console.log(typeof(b))
+
+let required = false 
+console.log(typeof(required)) */
+
+
+const flag = true 
+
+if(flag){
+console.log ("condition satisfied")
+}
+
+else {
+console.log("condition NOT satisfied")
+}
+
+let i = 0
+ while(i>10){
+i++
+console.log(i)
+ }
+
+ do {
+i++
+ } while(i>10);
+ console.log(i)
---
+++ tests/ComputerTest.spec.js
@@ -0,0 +1,18 @@
+const {test, expect} = require("@playwright/test");
+test('(e2e test) Successful PC creation', async ({page}) => {
+//open the web-site
+await page.goto('https://computer-database.gatling.io/computers')
+//click on the btn
+await page.getByText('Add a new computer').click();
+//fill form
+await page.locator('#name').fill('My-new-computer');
+await page.locator('#introduced').fill('1981-10-10');
+await page.locator('#discontinued').fill('2023-11-11');
+//select one available option
+await page.locator('#company').selectOption({label: 'Netronics'});
+//click on the btn Create this computer
+await page.getByText('Create this computer').click();
+//Verify
+await expect(page.getByText('Done ! Computer My-new-computer has been created')).toBeVisible();
+
+}, {timeout: 60000 });
