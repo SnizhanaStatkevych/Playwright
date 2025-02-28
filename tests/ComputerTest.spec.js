c5eb0ae2cc1b2b0d3e8bd7490ec6298de77345d0
SnizhanaStatkevych <snizhana.statkevych1501@gmail.com>
2025-02-28 13:09:11 +0200

    add new test

--- tests/ComputerTest.spec.js
+++ tests/ComputerTest.spec.js
@@ -1,4 +1,6 @@
-/* const {test, expect} = require("@playwright/test");
+const {test, expect} = require("@playwright/test");
+
+
 test('(e2e test) Successful PC creation', async ({page}) => {
 //open the web-site
 await page.goto('https://computer-database.gatling.io/computers')
@@ -15,7 +17,19 @@ await page.getByText('Create this computer').click();
 //Verify
 await expect(page.getByText('Done ! Computer My-new-computer has been created')).toBeVisible();
 
-}, {timeout: 60000 }); */
+}, {timeout: 60000 }); 
 
 
 
+test('Browser Context Playwright test', async ({browser}) => {
+
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    console.log(await page.title()); 
+
+    await page.locator('#username').type("rahulshetty");
+    await page.locator("[type='password]").type("learning");
+    await page.locator("#signInBtn").click();
+    await page.locator("[style*='block']");
+})
