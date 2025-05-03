679556281501cc98e166e60f204b9be0c258ddac
SnizhanaStatkevych <snizhana.statkevych1501@gmail.com>
2025-05-03 23:55:37 +0300

    New test for Rozetka (Login)

--- tests/ComputerTest.spec.js
+++ tests/ComputerTest.spec.js
@@ -21,3 +21,36 @@ import { test, expect } from '@playwright/test';
 
 
 
+/* test('Browser Context Playwright test', async ({browser}) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    const userName = page.locator('#username');
+    const signIn = page.locator("#signInBtn") ;
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    console.log(await page.title()); 
+// css
+    await userName.type("rahulshetty");
+    await page.locator("[type='password']").type("learning");
+    await signIn.click();
+    // wait untiol this locator show up page
+   console.log( await page.locator("[style*='block']").textContent());
+   await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+})
+
+// type = fill 
+await userName.fill("");
+await userName.fill("rahulshettyacademy");
+await signIn.click(); */ 
+
+
+
+
+test.only("Login Rozetka", async ({page}) => {
+
+    await page.goto ('https://comfy.ua/ua/?gad_source=1&gad_campaignid=9979162967&gbraid=0aaaaac-ixwgiozwbt-kjcuvvu42mxnfze&gclid=Cj0KCQjw_dbABhC5ARIsAAh2Z-SfX_FJ89hsgJkNEvlWRFHlQR9htWS25h5eCjT_Zlsz1cGnWrKPJH0aAn2sEALw_wcB'); 
+    await page.getByText('Увійти').first().click();
+    await page.getByPlaceholder('+38(___)___-__-__').fill('0978359552');
+    await page.locator("button[class = 'cmf-button-primary auth-init__sms']").click();
+
+    
+    },  {timeout: 60000 }); 
